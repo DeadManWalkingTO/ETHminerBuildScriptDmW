282b3c47425c7f92d02d1e4521b5f472e5d295fd
DeadManWalking <deadmanwalkingto@gmail.com>
2018-03-04 16:11:04 +0200

    Update ETHminerBuildScriptDmW.bat

--- ETHminerBuildScriptDmW.bat
+++ ETHminerBuildScriptDmW.bat
@@ -2,7 +2,7 @@ rem ========== PreStart ==========
 @echo off
 
 rem Set version info
-set V=1.4.2
+set V=1.5.1
 
 rem ========== Start ==========
 
@@ -33,7 +33,12 @@ rem Call Microsoft Visual C++ Build Tools
 echo ==================================================
 echo Call Microsoft Visual C++ Build Tools
 echo.
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
+if ERRORLEVEL 0 (goto :BT_OK) ELSE goto :BT_2015
+:BT_2015
 call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools_msbuild.bat"
+if not %ERRORLEVEL%==0 goto :ErrorHandling
+:BT_OK
 echo.
 echo Done
 echo ==================================================
@@ -45,6 +50,7 @@ echo ==================================================
 echo Back to Directory
 echo.
 cd "%~dp0\"
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -58,6 +64,7 @@ echo ==================================================
 echo Remove old Ethminer's Directory
 echo.
 rmdir /S /Q ethminer
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -68,7 +75,8 @@ rem Download Ethminer
 echo ==================================================
 echo Download Ethminer
 echo.
-git clone https://github.com/ethereum-mining/ethminer.git
+git clone --depth=1 --branch=master https://github.com/ethereum-mining/ethminer.git
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -80,7 +88,30 @@ echo ==================================================
 echo Change Directory to Ethminer's
 echo.
 cd ethminer
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
+echo Done
+echo ==================================================
+echo.
+timeout 1 > nul
+
+rem Git Fetch
+echo ==================================================
+echo Git Fetch
+echo.
+git fetch -q origin
+if not %ERRORLEVEL%==0 goto :ErrorHandling
+echo Done
+echo ==================================================
+echo.
+timeout 1 > nul
+
+rem Git Checkout
+echo ==================================================
+echo Git Checkout
+echo.
+git checkout -qf FETCH_HEAD
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo Done
 echo ==================================================
 echo.
@@ -91,6 +122,7 @@ echo ==================================================
 echo Git Submodule
 echo.
 git submodule update --init --recursive
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -102,6 +134,7 @@ echo ==================================================
 echo Make Build Directory
 echo.
 mkdir build
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -113,6 +146,7 @@ echo ==================================================
 echo Change Directory to Build
 echo.
 cd build
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -128,6 +162,7 @@ echo Stratum build ON -- DETHSTRATUM=ON
 echo API build ON -- DAPICORE=ON
 echo.
 cmake .. -G "Visual Studio 14 2015 Win64" -T v140,host=x64 -DETHASHCL=ON -DETHASHCUDA=ON -DETHSTRATUM=ON -DAPICORE=ON
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -139,6 +174,7 @@ echo ==================================================
 echo Build Ethminer
 echo.
 cmake --build . --config Release
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -150,6 +186,7 @@ echo ==================================================
 echo Copy ethminer to Home Directory
 echo.
 copy "%~dp0\ethminer\Release\ethminer.exe" "%~dp0\ethminer.exe"
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -161,6 +198,7 @@ echo ==================================================
 echo Change Directory to Home
 echo.
 cd "%~dp0\"
+if not %ERRORLEVEL%==0 goto :ErrorHandling
 echo.
 echo Done
 echo ==================================================
@@ -171,9 +209,19 @@ rem ETHminerBuildScriptDmW was completed
 echo ==================================================
 echo ETHminerBuildScriptDmW was completed
 echo ==================================================
+goto :EndProgram
+
+rem ========== Error Handling ==========
+
+:ErrorHandling
+
+echo ==================================================
+echo ETHminerBuildScriptDmW Error
+echo ==================================================
 
 rem ========== End ==========
 
+:EndProgram
 endlocal
 pause
 echo.
